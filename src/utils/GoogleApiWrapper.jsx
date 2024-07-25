1c7a02029b178432e506f2a0c873445302339089
Epris Richardson <33326194+eprisr@users.noreply.github.com>
2024-07-25 19:26:09 -0400

    Make GoogleApiWrapper Functional Component

--- src/utils/GoogleApiWrapper.jsx
+++ src/utils/GoogleApiWrapper.jsx
@@ -3,27 +3,25 @@ import React from 'react';
 import GoogleMap from '../Map';
 import MapError from '../MapError';
 // require('dotenv').config()
-export class MapContainer extends React.Component {
-	render() {
-		//https://www.fullstackreact.com/articles/how-to-write-a-google-maps-react-component/#the-map-container-component
-		//If the API has not loaded, display Loading...
-		//Wrapper passes a loaded state that changes to true once loaded
-		// if (!this.props.loaded) {
-		// 	return <div>Loading...</div>
-		// }
+export function MapContainer({ userInput, results, center, result }) {
+	//https://www.fullstackreact.com/articles/how-to-write-a-google-maps-react-component/#the-map-container-component
+	//If the API has not loaded, display Loading...
+	//Wrapper passes a loaded state that changes to true once loaded
+	// if (!this.props.loaded) {
+	// 	return <div>Loading...</div>
+	// }
 
-		return (
-			<div>
-				<GoogleMap
-					google={window.google}
-					userInput={this.props.userInput}
-					results={this.props.results}
-					result={this.props.result}
-					center={this.props.center}
-				/>
-			</div>
-		);
-	}
+	return (
+		<div>
+			<GoogleMap
+				google={window.google}
+				userInput={userInput}
+				results={results}
+				result={result}
+				center={center}
+			/>
+		</div>
+	);
 }
 
 // export default GoogleApiWrapper({
