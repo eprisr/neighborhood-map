5ff616e15b9f86a25ed846826371bb8454fcf266
Epris Richardson <33326194+eprisr@users.noreply.github.com>
2024-07-25 18:47:06 -0400

    Make Sidebar Functional Component

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -1,66 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './index.css';
 import Search from './Search';
 import MenuIcon from '@mui/icons-material/Menu';
 import { IconButton, List } from '@mui/material';
 
-class Sidebar extends React.Component {
-	state = {
-    mobileOpen: false,
+function Sidebar({ locations, getQuery, getNear, results, resultClicked }) {
+	const [mobileOpen, setMobileOpen] = useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(prevMobileState => !prevMobileState);
   };
 
-  handleDrawerToggle = () => {
-    this.setState(state => ({ mobileOpen: !state.mobileOpen }));
-  };
+	const drawer = (
+		<div>
+			<List>
+				<h1>SEARCH</h1>
 
-	render() {
-		const drawer = (
-			<div>
-        <List>
-          <h1 style={{ marginLeft: 10 }}>SEARCH</h1>
+				<Search
+					locations={locations}
+					getQuery={getQuery}
+					getNear={getNear}
+					results={results}
+					resultClicked={resultClicked}
+				/>
+			</List>
+		</div>
+	)
 
-          <Search
-            locations={this.props.locations}
-            getQuery={this.props.getQuery}
-            getNear={this.props.getNear}
-            results={this.props.results}
-            resultClicked={this.props.resultClicked}
-          />
-        </List>
-			</div>
-		);
-
-		return (
-			<div>
-				<IconButton
-					color="primary"
-					aria-label="Open drawer"
-					onClick={this.handleDrawerToggle}
+	return (
+		<div>
+			<IconButton
+				color="primary"
+				aria-label="Open drawer"
+				onClick={handleDrawerToggle}
+			>
+				<MenuIcon />
+			</IconButton>
+			<nav>
+				{/* <Drawer
+					container={this.props.container}
+					variant="temporary"
+					open={this.state.mobileOpen}
+					onClose={this.handleDrawerToggle}
+					ModalProps={{
+						keepMounted: true, // Better open performance on mobile. (-MaterialUI)
+					}}
 				>
-					<MenuIcon />
-				</IconButton>
-        <nav>
-					{/* <Drawer
-						container={this.props.container}
-						variant="temporary"
-						open={this.state.mobileOpen}
-						onClose={this.handleDrawerToggle}
-						ModalProps={{
-							keepMounted: true, // Better open performance on mobile. (-MaterialUI)
-						}}
-					>
-						{drawer}
-					</Drawer>
-					<Drawer
-						variant="permanent"
-						open
-					>
-						{drawer}
-					</Drawer> */}
-        </nav>
-			</div>
-		)
-	}
+					{drawer}
+				</Drawer>
+				<Drawer
+					variant="permanent"
+					open
+				>
+					{drawer}
+				</Drawer> */}
+			</nav>
+		</div>
+	)
 }
 
 export default Sidebar;
