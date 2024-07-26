a5d5964986deba0621e60bf20b82bd5f2d2a1195
Epris Richardson <33326194+eprisr@users.noreply.github.com>
2024-07-26 07:54:11 -0400

    Fix - Refactor App - Locations Null Error

--- src/App.jsx
+++ src/App.jsx
@@ -1,155 +1,108 @@
 import React, { useState, useEffect } from 'react';
 import { APIProvider } from '@vis.gl/react-google-maps';
-import Sidebar from './Sidebar';
-import './App.css';
 import Swal from 'sweetalert2';
 import escapeRegExp from 'escape-string-regexp';
+import Sidebar from './Sidebar';
 import GoogleMap from './Map';
+import './App.css';
 
 const MAPS_API_KEY = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
 const FOURSQUARE_API_KEY = import.meta.env.VITE_FOURSQUARE_API_KEY;
 
-// const client_id = import.meta.env.FOURSQUARE_CLIENT_ID;
-// const client_secret = import.meta.env.FOURSQUARE_CLIENT_SECRET;
-// const v = '20180323';
-const options = {
-  method: 'GET',
-  headers: {
-    Accept: 'application/json',
-    Authorization: `${FOURSQUARE_API_KEY}`,
-  },
-}
 function App() {
-	const [locations, setLocations] = useState(null);
-	const [results, setResults] = useState([]);
-	const [result, setResult] = useState({});
-	const [userInputValue, setUserInputValue] = useState({ near: '' });
-	const [center, setCenter] = useState({ lat: 41.85003, lng: -87.65005 });
-	const [dataComplete, setDataComplete] = useState(false);
-	
-  //Fetch data when component mounts
-  useEffect(() => {
-		getData('Chicago, IL')
-  })
+  const [locations, setLocations] = useState(null)
+  const [results, setResults] = useState([])
+  const [result, setResult] = useState({})
+  const [userInputValue, setUserInputValue] = useState({ near: '' })
+  const [center, setCenter] = useState({ lat: 41.85003, lng: -87.65005 })
+	const [dataComplete, setDataComplete] = useState(false)
 
-  //Fetch data function
-  const getData = (near) => {
-    setDataComplete(false)
-  //Fetch locations from FourSquare Search API
-    fetch(`https://api.foursquare.com/v3/places/search?query=smoothie&near=${near}&sort=DISTANCE&limit=50`, options)
-      .then((response) => {
-        //If response is an error due to query...
-        if(!response.ok) {
-          throw Error(console.log)
-        }
-
-        //Successful response
-        return response.json()
-      })
-      //Update locations array from data
-			.then((response) => {
-				setLocations(response.results)
-      })
-      .catch((error) =>
-				Swal.fire({
-					icon: "error",
-					title: "ERROR!",
-					text: "Unable to Retrieve Data.",
-				})
-      );
+	function getData(near) {
+		setDataComplete(false);
+		fetch(
+      `https://api.foursquare.com/v3/places/search?query=smoothie&near=${near}&sort=DISTANCE&limit=50`,
+      {
+        method: 'GET',
+        headers: {
+          Accept: 'application/json',
+          Authorization: `${FOURSQUARE_API_KEY}`,
+        },
+      }
+		).then((res) => {
+			return res.json()
+		}).then((res) => {
+			setLocations(res.results)
+			setResults(res.results)
+			setDataComplete(true)
+		}).catch((err) => {
+			console.log(err)
+			Swal.fire({
+				title: "ERROR!",
+				text: "Unable to Retrieve Data.",
+				icon: "error",
+				showCloseButton: true,
+			})
+		}
+		)
 	}
-	
-	useEffect(() => {
-		if (results.length === 0) setResults(locations)
 
-    updateMap()
+	const userInput = (near) => {
+		setUserInputValue({ near });
+		getData(near);
+	}
+
+	const getQuery = (query) => {
+		if (query !== '' && query !== undefined) {
+			const venue = new RegExp(escapeRegExp(query), 'i');
+			setResults(locations.filter((location) => venue.test(location.name)))
+		} else {
+			setResults(locations)
+		}
+	}
+
+	const getNear = (near) => {
+		userInput(near);
+	}
+
+	const getResult = (r) => {
+		const index = results.indexOf(r);
+		setResult({ result: r, index });
+	}
+
+	useEffect(() => {
+		getData('Chicago, IL')
+	}, [])
+
+	useEffect(() => {
+		if (locations !== null && locations[0] !== undefined) {
+      const lat = locations[0].geocodes.main.latitude
+      const lng = locations[0].geocodes.main.longitude
+      setCenter({ lat, lng })
+    }
 	}, [locations])
 	
 
-  //Grab query from search by venue name input
-  const getQuery = (query) => {
-    if( query !== '' && query !== undefined) {
-      const venue = new RegExp(escapeRegExp(query), 'i');
-		  // this.setState((state) => ({
-      //   results: state.locations.filter((location) => venue.test(location.name))
-      // }) );
-		  setResults(locations.filter((location) => venue.test(location.name)));
-		} else {
-      setResults(locations)
-    }
-  }
-
-  //Grab near from search by location input
-  const getNear = (near) => {
-    userInput(near);
-  }
-
-  const getResult = (r) => {
-    let index = results.indexOf(r)
-    setResult({ result: r, index: index })
-  }
-
-  const userInput = (near) => {
-    // this.setState({ userInputValue: {near: near} }, () => {
-    //   this.getData(near);
-		// })
-		setUserInputValue({ near: near })
-	}
-	
-	useEffect(() => {
-		getData()
-	}, [userInputValue])
-	
-
-  const updateMap = () =>{
-    updateCenter()
-    updateResults()
-  }
-
-  const updateCenter = () => {
-    if(locations[0] !== undefined || locations !== null) {
-      let lat = locations[0].geocodes.main.latitude
-      let lng = locations[0].geocodes.main.longitude
-      setCenter({lat: lat, lng: lng })
-    } else {
-      return;
-    }
-  }
-
-  const updateResults = () => {
-    setResults(locations)
-    setDataComplete(true)
-  }
-
-	let retrievedData = dataComplete;
-
-	return (
-		<div className="App">
-			<Sidebar
-				locations={locations}
-				getQuery={getQuery}
-				getNear={getNear}
-				results={results}
-				resultClicked={getResult}
+  return (
+    <div className="App">
+      <Sidebar
+        locations={locations}
+        getQuery={getQuery}
+        getNear={getNear}
+        results={results}
+        resultClicked={getResult}
 			/>
-			<APIProvider apiKey={MAPS_API_KEY}>
-				<GoogleMap
-					userInput={userInputValue}
-					results={results}
-					center={center}
-					result={result}
-				/>
-			</APIProvider>
-			{/* { retrievedData === true &&
-				<GoogleApiWrapper
-					userInput={userInputValue}
-					results={results}
-					center={center}
-					result={result}
-				/>
-			} */}
-		</div>
-	)
+			{dataComplete &&
+				<APIProvider apiKey={MAPS_API_KEY}>
+					<GoogleMap
+						userInput={userInputValue}
+						results={results}
+						center={center}
+						result={result}
+					/>
+				</APIProvider>
+			}
+    </div>
+  )
 }
 
 export default App;
