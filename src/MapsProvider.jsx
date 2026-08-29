300df1e9faed52ac904fabcb0412cdcf4dfbe116
eprisr <eharrisburnett@gmail.com>
2026-08-29 12:48:49 -0400

    Import Google Map in Provider

--- src/MapsProvider.jsx
+++ src/MapsProvider.jsx
@@ -1,4 +1,5 @@
 import { APIProvider } from '@vis.gl/react-google-maps'
+import GoogleMap from './components/Map'
 
 function MapsProvider({ userInputValue, results, center, result }) {
 	const MAPS_API_KEY = import.meta.env.GOOGLE_MAPS_DEMO_KEY
