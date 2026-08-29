c950ef0e7da69e7cb23ca1bbdd216c274962a464
eprisr <eharrisburnett@gmail.com>
2026-08-29 12:48:12 -0400

    Update Fsq ID

--- src/components/Suggestions.jsx
+++ src/components/Suggestions.jsx
@@ -1,32 +1,32 @@
-import React, { useState } from 'react';
-import { Grid, List, ListItem, ListItemButton } from '@mui/material';
+import React, { useState } from 'react'
+import { Grid, List, ListItem, ListItemButton } from '@mui/material'
 
 function Suggestions({ results, getResult }) {
 	const [selectedIndex, setSelectedIndex] = useState(null)
-	
+
 	const clickResult = (e, i) => {
 		setSelectedIndex(i)
 	}
 
 	return (
-    <List>
-      {results && results.map((r) => (
-        <ListItem key={r.fsq_id}>
-					<ListItemButton
-						selected={selectedIndex === r.fsq_id}
-						onClick={(e) => {
-							getResult(r);
-							clickResult(e, r.fsq_id)
-						}}
-          >
-						<Grid container spacing={2}>
-							<Grid size={12}>{r.name}</Grid>
-						</Grid>
-          </ListItemButton>
-        </ListItem>
-      ))}
-    </List>
-  )
+		<List>
+			{results &&
+				results.map((r) => (
+					<ListItem key={r.fsq_place_id}>
+						<ListItemButton
+							selected={selectedIndex === r.fsq_place_id}
+							onClick={(e) => {
+								getResult(r)
+								clickResult(e, r.fsq_place_id)
+							}}>
+							<Grid container spacing={2}>
+								<Grid size={12}>{r.name}</Grid>
+							</Grid>
+						</ListItemButton>
+					</ListItem>
+				))}
+		</List>
+	)
 }
 
-export default Suggestions;
+export default Suggestions
